f2b246e6925b6d9bcb315be1b6e98c976bd43fbf
JOSHUA DRAGE <j.drage.21@abdn.ac.uk>
2022-06-01 14:44:04 +0000

    New Webpage Tenmplates

--- airMonitor/urls.py
+++ airMonitor/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('sensor/', views.sensor, name='sensor'),
+	path('sensorList/', views.sensorList, name='sensorList'),
+	path('comparisons/', views.comparisons, name='comparisons'),
+
 ]
--- airMonitor/views.py
+++ airMonitor/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request, 'airMonitor/index.html')
+
+def sensor(request):
+	return render(request, 'airMonitor/sensor.html')
+
+def sensorList(request):
+	return render(request, 'airMonitor/sensorList.html')
+
+def comparisons(request):
+	return render(request, 'airMonitor/comparisons.html')	
