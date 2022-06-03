e0c1f201bbf0ae3f4878fd69fd592577e886ef39
JOSHUA DRAGE <j.drage.21@abdn.ac.uk>
2022-06-03 14:48:07 +0000

    Comparison Template created

--- airMonitor/views.py
+++ airMonitor/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import  render, redirect
+from .models import sensorList, sensor1
 # Create your views here.
 def index(request):
 	return render(request, 'airMonitor/index.html')
@@ -7,9 +7,20 @@ def index(request):
 def sensor(request):
 	return render(request, 'airMonitor/sensor.html')
 
-def sensorList(request):
+def sensorListPage(request):
 	sensors = sensorList.objects.all()
 	return render(request, 'airMonitor/sensorList.html', {'sensors' : sensors})
 
 def comparisons(request):
-	return render(request, 'airMonitor/comparisons.html')	
+	sensors = sensorList.objects.all()		
+	if request.method == "POST":
+		s1 = request.POST.get('sensor1','')
+		s2 = request.POST.get('sensor2','')
+		sList1 = sensorList.objects.get(sensorID=s1)
+		sList2 = sensorList.objects.get(sensorID=s2)
+		context = 	{	'sensors' : sensors,
+						'sList1' : sList1,
+						'sList2' : sList2,
+					}
+		return render(request, 'airMonitor/comparisons.html', context)	
+	return render(request, 'airMonitor/comparisons.html', {'sensors' : sensors})	
