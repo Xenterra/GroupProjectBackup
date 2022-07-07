63ea0ff57074e9c8c3d545238e99292544b34f63
JOSHUA DRAGE <j.drage.21@abdn.ac.uk>
2022-07-07 16:18:12 +0000

    Migrations for a fresh db.sqlite file

--- airMonitor/management/commands/DataPull.py
+++ airMonitor/management/commands/DataPull.py
@@ -13,120 +13,120 @@ class Command(BaseCommand):
 	help = 'Load data from json'
 	def handle(self, *args, **options):
 		#sensorList.objects.all().delete()
-		BME280Reading.objects.all().delete()
-		SDS011Reading.objects.all().delete()
-		DHT22Reading.objects.all().delete()
+		#BME280Reading.objects.all().delete()
+		#SDS011Reading.objects.all().delete()
+		#DHT22Reading.objects.all().delete()
 
-#		json_url = requests.get("https://raw.githubusercontent.com/scharlau/Air_Aberdeen_Back_End/main/data/bq_data.json")
-#		data = json_url.json()
-#		#print(data)
-#
-#		listOfSensorID=[]
-#		for x in data:
-#			listOfSensorID.append(""+x["sensor_id"]+","+x["sensor_type"]+","+x["latitude"]+","+x["longitude"]+","+x["location_id"])
-#		listOfSensorID = list(set(listOfSensorID))
-#
-#		for newSensor in listOfSensorID:
-#			arr = newSensor.split(',')
-#			try:
-#				sList = sensorList.objects.create(
-#					sensorID = arr[0],
-#					sensorType = arr[1],
-#					latitude  = arr[2],
-#					longitude = arr[3],
-#					location_id = arr[4],
-#					)
-#				sList.save()
-#				print("Completed Row:",arr[0])
-#			except:
-#  				print(arr[0],"This sensor exists")
-#
-#		for y in data:
-#			newTemperature = 0.0
-#			newHumidity = 0.0
-#			newPressure = 0.0
-#			newP1 = 0.0
-#			newP2 = 0.0
-#			if y["sensor_type"] == "BME280":
-#				shortDate = y["timestamp"]+"-00:00"
-#				try:
-#					if math.isnan(float(y["humidity"])):
-#						newHumidity = 100.0
-#					else:
-#						newHumidity = 0.0+float(y["humidity"])
-#				except:
-#					newHumidity = "100.0"
-#				try:
-#					if math.isnan(float(y["pressure"])):
-#						newPressure = 0.0
-#					else:
-#						newPressure = 0.0+float(y["pressure"])
-#				except:
-#					newPressure = "0.0"
-#				try:
-#					if math.isnan(float(y["temperature"])):
-#						newTemperature = 0.0
-#					else:
-#						newTemperature = 0.0+float(y["temperature"])
-#				except:
-#					newTemperature = "0.0"
-#
-#				sList = BME280Reading.objects.create(
-#						sensorID = sensorList.objects.get(sensorID=y["sensor_id"]),
-#						timestamp = shortDate,
-#						humidity  = newHumidity,
-#						pressure = newPressure,
-#						temperature = newTemperature,
-#						)
-#				print("Completed BME280 Reading:",y["sensor_id"],shortDate)
-#
-#			elif y["sensor_type"] == "SDS011":
-#				shortDate = y["timestamp"]+"-00:00"				
-#				try:
-#					if math.isnan(float(y["P1"])):
-#						newP1 = 0.0
-#					else:
-#						newP1 = 0.0+float(y["P1"])
-#				except:
-#					newP1 = "0.0"
-#				try:
-#					if math.isnan(float(y["P2"])):
-#						newP2 = 0.0
-#					else:
-#						newP2 = 0.0+float(y["P2"])
-#				except:
-#					newP2 = "0.0"
-#
-#				sList = SDS011Reading.objects.create(
-#						sensorID = sensorList.objects.get(sensorID=y["sensor_id"]),
-#						timestamp = shortDate,
-#						P1  = newP1,
-#						P2 = newP2,
-#						)
-#				print("Completed SDS011 Reading:",y["sensor_id"],shortDate)
-#			elif y["sensor_type"] == "DHT22":
-#				shortDate = y["timestamp"]+"-00:00"
-#				try:
-#					if math.isnan(float(y["humidity"])):
-#						newHumidity = 100.0
-#					else:
-#						newHumidity = 0.0+float(y["humidity"])
-#				except:
-#					newHumidity = "100.0"
-#				try:
-#					if math.isnan(float(y["temperature"])):
-#						newTemperature = 0.0
-#					else:
-#						newTemperature = 0.0+float(y["temperature"])
-#				except:
-#					newTemperature = "0.0"
-#
-#				sList = DHT22Reading.objects.create(
-#						sensorID = sensorList.objects.get(sensorID=y["sensor_id"]),
-#						timestamp = shortDate,
-#						humidity  = newHumidity,
-#						temperature = newTemperature,
-#						)
-#				print("Completed DHT22 Reading:",y["sensor_id"],shortDate)
-#			else:
-#				print("Invalid Sensor Type")
+		json_url = requests.get("https://raw.githubusercontent.com/scharlau/Air_Aberdeen_Back_End/main/data/bq_data.json")
+		data = json_url.json()
+		#print(data)
+
+		listOfSensorID=[]
+		for x in data:
+			listOfSensorID.append(""+x["sensor_id"]+","+x["sensor_type"]+","+x["latitude"]+","+x["longitude"]+","+x["location_id"])
+		listOfSensorID = list(set(listOfSensorID))
+
+		for newSensor in listOfSensorID:
+			arr = newSensor.split(',')
+			try:
+				sList = sensorList.objects.create(
+					sensorID = arr[0],
+					sensorType = arr[1],
+					latitude  = arr[2],
+					longitude = arr[3],
+					location_id = arr[4],
+					)
+				sList.save()
+				print("Completed Row:",arr[0])
+			except:
+  				print(arr[0],"This sensor exists")
+
+		for y in data:
+			newTemperature = 0.0
+			newHumidity = 0.0
+			newPressure = 0.0
+			newP1 = 0.0
+			newP2 = 0.0
+			if y["sensor_type"] == "BME280":
+				shortDate = y["timestamp"]+"-00:00"
+				try:
+					if math.isnan(float(y["humidity"])):
+						newHumidity = 100.0
+					else:
+						newHumidity = 0.0+float(y["humidity"])
+				except:
+					newHumidity = "100.0"
+				try:
+					if math.isnan(float(y["pressure"])):
+						newPressure = 0.0
+					else:
+						newPressure = 0.0+float(y["pressure"])
+				except:
+					newPressure = "0.0"
+				try:
+					if math.isnan(float(y["temperature"])):
+						newTemperature = 0.0
+					else:
+						newTemperature = 0.0+float(y["temperature"])
+				except:
+					newTemperature = "0.0"
+
+				sList = BME280Reading.objects.create(
+						sensorID = sensorList.objects.get(sensorID=y["sensor_id"]),
+						timestamp = shortDate,
+						humidity  = newHumidity,
+						pressure = newPressure,
+						temperature = newTemperature,
+						)
+				print("Completed BME280 Reading:",y["sensor_id"],shortDate)
+
+			elif y["sensor_type"] == "SDS011":
+				shortDate = y["timestamp"]+"-00:00"				
+				try:
+					if math.isnan(float(y["P1"])):
+						newP1 = 0.0
+					else:
+						newP1 = 0.0+float(y["P1"])
+				except:
+					newP1 = "0.0"
+				try:
+					if math.isnan(float(y["P2"])):
+						newP2 = 0.0
+					else:
+						newP2 = 0.0+float(y["P2"])
+				except:
+					newP2 = "0.0"
+
+				sList = SDS011Reading.objects.create(
+						sensorID = sensorList.objects.get(sensorID=y["sensor_id"]),
+						timestamp = shortDate,
+						P1  = newP1,
+						P2 = newP2,
+						)
+				print("Completed SDS011 Reading:",y["sensor_id"],shortDate)
+			elif y["sensor_type"] == "DHT22":
+				shortDate = y["timestamp"]+"-00:00"
+				try:
+					if math.isnan(float(y["humidity"])):
+						newHumidity = 100.0
+					else:
+						newHumidity = 0.0+float(y["humidity"])
+				except:
+					newHumidity = "100.0"
+				try:
+					if math.isnan(float(y["temperature"])):
+						newTemperature = 0.0
+					else:
+						newTemperature = 0.0+float(y["temperature"])
+				except:
+					newTemperature = "0.0"
+
+				sList = DHT22Reading.objects.create(
+						sensorID = sensorList.objects.get(sensorID=y["sensor_id"]),
+						timestamp = shortDate,
+						humidity  = newHumidity,
+						temperature = newTemperature,
+						)
+				print("Completed DHT22 Reading:",y["sensor_id"],shortDate)
+			else:
+				print("Invalid Sensor Type")
