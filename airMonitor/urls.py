4b322b3ccf8f058d2c858e6680e3b3eee91d3e68
JOSHUA DRAGE <j.drage.21@abdn.ac.uk>
2022-07-08 17:48:53 +0000

    Chart Backgrounds and Link Repairs

--- airMonitor/urls.py
+++ airMonitor/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
+	path('', views.landing, name='landing'),
+	path('map/', views.index, name='index'),
 	path('sensor/', views.sensor, name='sensor'),
 	path('sensorListPage/', views.listPage, name='listPage'),
 	path('comparisons/', views.comparisons, name='comparisons'),
-	path('landing/', views.landing, name='landing'),
 
 ]
