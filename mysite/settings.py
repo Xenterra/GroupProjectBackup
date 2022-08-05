d52e98c935b2aeb5e5ddaf85131b0604d2edfb84
JOSHUA DRAGE <j.drage.21@abdn.ac.uk>
2022-08-05 15:02:46 +0000

    static fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,6 +127,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-  '/static/',
-)
