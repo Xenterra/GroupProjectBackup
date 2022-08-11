64584cad3e14b6e289da1f0a8ef1a19e2dde3e55
ADWOA SERWA ADDAI <a.addai.21@abdn.ac.uk>
2022-08-11 10:06:29 +0000

    download user guide

--- airMonitor/views.py
+++ airMonitor/views.py
@@ -4,7 +4,11 @@ from datetime import datetime, timedelta
 import pytz
 from django.shortcuts import  render, redirect
 from airMonitor.models import sensorList, BME280Reading, SDS011Reading, DHT22Reading, PastBME280Readings, PastDHT22Readings, PastSDS011Readings
-
+import mimetypes
+# import os module
+import os
+# Import HttpResponse module
+from django.http.response import HttpResponse
 # Create your views here.
 
 
@@ -415,8 +419,26 @@ def comparisons(request):
 def landing(request):
 	return render(request, 'airMonitor/landing.html')
 
-def User_guide(request):
-	return render(request, 'airMonitor/User_guide.html')
+def User_guide(request, filename=''):
+	#if filename != '':
+		BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        #  full file path
+		filename= 'User_manual.pdf'
+		filepath = BASE_DIR + '/airMonitor/files/' + filename
+        # this Open the file for reading content
+		path = open(filepath, 'rb')
+        # this Set the mime type
+		mime_type, _ = mimetypes.guess_type(filepath)
+        # Set the return value of the HttpResponse
+		response = HttpResponse(path, content_type=mime_type)
+        # Set the HTTP header for sending to browser
+		response['Content-Disposition'] = "attachment; filename=%s" % filename
+        # Return the response value
+		return response
+	#else:
+		#return render(request,'airMonitor/User_guide.html')
+		return render(request,'airMonitor/User_guide.html')
 
 def FAQs(request):
 	return render(request, 'airMonitor/FAQs.html')
+
